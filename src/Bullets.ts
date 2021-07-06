b45b8381e3f562c936c333d760c1e2db4d26d2c7
Year 12 <year12@Randwicks-MacBook-Air.local>
2021-07-06 14:59:16 +0100

    modularised bullets

--- src/Bullets.ts
+++ src/Bullets.ts
@@ -1,15 +1,36 @@
-/*import { GameObject } from './GameObject';
-import {Body, World, Bodies, Render} from 'matter-js';import * as PIXI from 'pixi.js'
-import { avatar,canvas, engine } from './index'
+import { GameObject } from "./GameObject";
+import { Body, Bodies, World, Engine  } from 'matter-js'
+import * as PIXI from 'pixi.js'
+import { canvas, engine, avatar } from './index'
 
-let bullets: any = []; //create an empty array to store bullets in
+export let bullets: Bullet[] = []; //create an empty array to store bullets in
 
-class Bullet extends GameObject { //creates a bullet class 
+export class Bullet extends GameObject { //creates a bullet class 
     speed: number;
-    constructor (pixiData:any, matterData:any, speed:number) {
+    dead:boolean;
+    constructor (pixiData:any, matterData:any, speed:number, dead:boolean) {
         super(pixiData, matterData)
         this.speed = speed;
+        this.dead = dead;
     }
+    update(delta:number) {
+        this.pixiData.position.x = this.matterData.position.x; 
+        this.pixiData.position.y = this.matterData.position.y; 
+        this.pixiData.rotation = this.matterData.angle;
+        this.pixiData.anchor.set(0.5);
+        for (let i = 0; i < bullets.length; i++) {
+            Body.setVelocity(bullets[i].matterData, {x:-bullets[i].speed, y:0});; //bullets move to the right when x is pressed
+            if (bullets[i].matterData.position.x > 800 || bullets[i].matterData.position.x < 0) { //if bullets have moved too far, then they are dead.
+                bullets[i].dead = true;
+            }
+            if (bullets[i].dead) { //removes bullets that are out of screen.
+                canvas.stage.removeChild(bullets[i].pixiData);
+                bullets.splice(i, 1); //removes dead bullets from array
+    
+            }
+        }
+    }
+
 }
 
 export function fire(left: boolean) {
@@ -18,27 +39,20 @@ export function fire(left: boolean) {
     bullets.push(bullet); // adds bullets that have been fired to an array of bullets
 }
 
-function createBullet(left:boolean) { // is responsible for creating the bullets
-    let bullet = new Bullet(PIXI.Sprite.from("assets/bullet.png"), Bodies.rectangle(avatar.pixiData.position.x, avatar.pixiData.position.y , 30, 20, {isStatic:true}), 10)
+export function createBullet(left:boolean) { // is responsible for creating the bullets
     if(left) { // by using a parameter, the program decides whether or not the bullet is travelling left.
+        let bullet = new Bullet(PIXI.Sprite.from("assets/bullet.png"), Bodies.rectangle(avatar.pixiData.position.x+35, avatar.pixiData.position.y , 30, 20, {isStatic:false}), 10, false);
         bullet.speed = -bullet.speed;
-    }
-    World.add(engine.world,[bullet.matterData])
-    canvas.stage.addChild(bullet.pixiData); //renders bullet but only pixiData can be rendered by pixi
+        World.add(engine.world,[bullet.matterData]);
+        canvas.stage.addChild(bullet.pixiData); 
 
-    return bullet;
-}
-export function updateBullets() {
-    for (let i = 0; i < bullets.length; i++) {
-        Body.setVelocity(bullets[i].matterData, {x:-bullets[i].speed, y:0});; //bullets move to the right when x is pressed
-        if (bullets[i].matterData.position.x > 800 || bullets[i].matterData.position.x < 0) { //if bullets have moved too far, then they are dead.
-            bullets[i].dead = true;
-        }
-        if (bullets[i].dead) { //removes bullets that are out of screen.
-            canvas.stage.removeChild(bullets[i].pixiData);
-            bullets.splice(i, 1); //removes dead bullets from array
+        return bullet;
+    } else {
+        let bullet = new Bullet(PIXI.Sprite.from("assets/bullet.png"), Bodies.rectangle(avatar.pixiData.position.x-35, avatar.pixiData.position.y , 30, 20, {isStatic:false}), 10, false);
+        bullet.speed = bullet.speed;
+        World.add(engine.world,[bullet.matterData]);
+        canvas.stage.addChild(bullet.pixiData); 
 
-        }
+        return bullet;
     }
 }
-*/
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as PIXI from 'pixi.js'
 import { Application, Loader, LoaderResource, PlaneGeometry, Rectangle, Sprite, Texture } from 'pixi.js'
 import {Engine, Body, World, Bodies, Render} from 'matter-js';
 import { Wall } from './Walls'
+import { Bullet, bullets, fire} from './bullets'
 
 export const engine = Engine.create();
 const loader = PIXI.Loader
@@ -51,59 +52,6 @@ function keysUp(e: any) {
     keys[e.keyCode] = false;
 }
 
-let bullets: Bullet[] = []; //create an empty array to store bullets in
-
-class Bullet extends GameObject { //creates a bullet class 
-    speed: number;
-    dead:boolean;
-    constructor (pixiData:any, matterData:any, speed:number, dead:boolean) {
-        super(pixiData, matterData)
-        this.speed = speed;
-        this.dead = dead;
-    }
-    update(delta:number) {
-        this.pixiData.position.x = this.matterData.position.x; 
-        this.pixiData.position.y = this.matterData.position.y; 
-        this.pixiData.rotation = this.matterData.angle;
-        this.pixiData.anchor.set(0.5);
-        for (let i = 0; i < bullets.length; i++) {
-            Body.setVelocity(bullets[i].matterData, {x:-bullets[i].speed, y:0});; //bullets move to the right when x is pressed
-            if (bullets[i].matterData.position.x > 800 || bullets[i].matterData.position.x < 0) { //if bullets have moved too far, then they are dead.
-                bullets[i].dead = true;
-            }
-            if (bullets[i].dead) { //removes bullets that are out of screen.
-                canvas.stage.removeChild(bullets[i].pixiData);
-                bullets.splice(i, 1); //removes dead bullets from array
-    
-            }
-        }
-    }
-
-}
-
-function fire(left: boolean) {
-    console.log("Fire!"); //for checking that the bullets are actually firing. will remove when project is finished.
-    let bullet = createBullet(left); //calls createBullet function.
-    bullets.push(bullet); // adds bullets that have been fired to an array of bullets
-}
-
-function createBullet(left:boolean) { // is responsible for creating the bullets
-    if(left) { // by using a parameter, the program decides whether or not the bullet is travelling left.
-        let bullet = new Bullet(PIXI.Sprite.from("assets/bullet.png"), Bodies.rectangle(avatar.pixiData.position.x+35, avatar.pixiData.position.y , 30, 20, {isStatic:false}), 10, false);
-        bullet.speed = -bullet.speed;
-        World.add(engine.world,[bullet.matterData]);
-        canvas.stage.addChild(bullet.pixiData); 
-
-        return bullet;
-    } else {
-        let bullet = new Bullet(PIXI.Sprite.from("assets/bullet.png"), Bodies.rectangle(avatar.pixiData.position.x-35, avatar.pixiData.position.y , 30, 20, {isStatic:false}), 10, false);
-        bullet.speed = bullet.speed;
-        World.add(engine.world,[bullet.matterData]);
-        canvas.stage.addChild(bullet.pixiData); 
-
-        return bullet;
-    }
-}
 let lastBulletTime:number = null;
 function gameLoop(delta:number) {
     //Z makes the player go up the screen by giving the avatar an upwards velocity.
@@ -115,8 +63,8 @@ function gameLoop(delta:number) {
             lastBulletTime = now;
         }
     }
-    //X          
-    if (keys["67"]) {
+    //C         
+    if (keys["88"]) {
         let now = Date.now(); //number of milliseconds since 1/1/1970
 
         if ((now - lastBulletTime) > 300) { //lastBulletTime is initially 0, so this will always fire straight away
@@ -124,8 +72,8 @@ function gameLoop(delta:number) {
             lastBulletTime = now; //updates the last time a bullet was fired.
         }
     }
-    //C
-    if (keys["88"]) {
+    //X
+    if (keys["67"]) {
         let now = Date.now();
 
         if ((now - lastBulletTime) > 300) {
