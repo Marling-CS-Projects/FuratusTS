7e2481540b169988bcd6d5f6d812d60468b1fad2
Year 12 <year12@Randwicks-MacBook-Air.local>
2021-07-12 21:51:34 +0100

    tried adding an end case

--- src/index.ts
+++ src/index.ts
@@ -55,24 +55,40 @@ function keysUp(e: any) {
 
 //collisions for the player being grounded
 let playerGrounded:boolean;
-let walls, [] = [];
+
 let test:Matter.Body;
-Matter.Events.on(engine, "collisionStart", function (event) { //when Matter detects a collison start
-    event.pairs
-        .filter(pair => pair.bodyA == avatar.matterData || pair.bodyB == avatar.matterData) //filter with avatar as bodyA or bodyB
-        .forEach(pair => {
-            let possibleGrounding = pair.bodyA == avatar.matterData ? pair.bodyB : pair.bodyA; //checks if the avatar is bodyA or B
-            if (possibleGrounding == bottomWall.matterData) { //if they are colliding, then the player is on the ground.
-                playerGrounded = true;
-            }
-        });
-});
+function jump() {
+    Matter.Events.on(engine, "collisionStart", function (event) { //when Matter detects a collison start
+        event.pairs
+            .filter(pair => pair.bodyA == avatar.matterData || pair.bodyB == avatar.matterData) //filter with avatar as bodyA or bodyB
+            .forEach(pair => {
+                let possibleGrounding = pair.bodyA == avatar.matterData ? pair.bodyB : pair.bodyA; //checks if the avatar is bodyA or B
+                if (possibleGrounding == bottomWall.matterData) { //if they are colliding, then the player is on the ground.
+                    playerGrounded = true;
+                }
+            })
+    })
+    Matter.Events.on(engine, "collisionEnd", function (event) { //when Matter detects a collison start
+        event.pairs
+            .filter(pair => pair.bodyA == avatar.matterData || pair.bodyB == avatar.matterData) //filter with avatar as bodyA or bodyB
+            .forEach(pair => {
+                let possibleGrounding = pair.bodyA == avatar.matterData ? pair.bodyB : pair.bodyA; //checks if the avatar is bodyA or B
+                if (possibleGrounding == bottomWall.matterData) { //if they are colliding, then the player is on the ground.
+                    playerGrounded = false;
+                }
+            })
+    })
+    if  (playerGrounded = true) {
+        Body.setVelocity(avatar.matterData, {x:0, y: -5})
+    }
+}
+
 let lastBulletTime:number = null;
 let lastJumpTime:number = null;
 function gameLoop(delta:number) {
     //Z makes the player 'jump' by giving the avatar an upwards velocity.
     if (keys["90"]) {
-        Body.setVelocity(avatar.matterData, {x:0, y:-5})
+        jump();
     }
     //C         
     if (keys["88"]) {
